ddc747b5468049ff9ac3cd48d0cd5807592d7b09
Amanda Caroline da Silva Rodrigues <amandacsr10@gmail.com>
2025-10-16 09:38:32 -0300

    edited vsi to add resource group

--- modules/vsi/main.tf
+++ modules/vsi/main.tf
@@ -1,7 +1,11 @@
+data "ibm_resource_group" "rg" {
+  name = var.resource_group
+}
+
 resource "ibm_is_virtual_network_interface" "vni" {
   name           = var.vni_name
   subnet         = var.vni_subnet
-  resource_group = var.resource_group_id
+  resource_group = data.ibm_resource_group.rg.id
   tags           = var.tags
 }
 
@@ -12,6 +16,7 @@ data "ibm_is_image" "image" {
 
 resource "ibm_is_instance" "vsi" {
   name    = var.vsi_name
+  resource_group = data.ibm_resource_group.rg.id
   image   = data.ibm_is_image.image.id
   zone    = var.zone
   profile = var.vsi_profile
--- modules/vsi/variables.tf
+++ modules/vsi/variables.tf
@@ -1,8 +1,9 @@
 ### Compartilháveis
 
-variable "resource_group_id" {
-  description = "The ID of the resource group the VNI will be created"
+variable "resource_group" {
+  description = "The resource group where the VPC to be created"
   type        = string
+  default     = "Default"
 }
 
 variable "tags" {
